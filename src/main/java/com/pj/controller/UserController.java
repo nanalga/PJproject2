047fc234c4300de2a5fc9ed6ac7b5aa68db6ba75
Anjinhyeok <75560997+millionfood@users.noreply.github.com>
2021-12-24 17:06:27 +0900

    connect db

--- src/main/java/com/pj/controller/UserController.java
+++ src/main/java/com/pj/controller/UserController.java
@@ -1,5 +1,26 @@
 package com.pj.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.pj.service.UserService;
+
+import lombok.Setter;
+
+@Controller
+@RequestMapping("/user")
 public class UserController {
 	
+	@Setter(onMethod_ = @Autowired)
+	private UserService userSerivce;
+	
+	@RequestMapping("")
+	public String info(Model model) {
+		
+		String userInfo = userSerivce.getUserInfo();
+		model.addAttribute("info", userInfo);
+		return "user/info";
+	}
 }
