1fe00b506b52215877c2c8abe24c943945f54635
GGiYeol <niceky081818@naver.com>
2021-12-30 15:47:02 +0900

    park first commit

--- src/main/java/com/pj/controller/CommunityController.java
+++ src/main/java/com/pj/controller/CommunityController.java
@@ -1,5 +1,23 @@
 package com.pj.controller;
 
-public class CommunityController {
+import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/comBoard")
+public class CommunityController {
+	
+	@RequestMapping("/")
+	public String main(HttpServletRequest request) {
+	
+	return "community/community";
+	}
+	
+	@RequestMapping("/free")
+	public String free(HttpServletRequest request) {
+	
+	return "community/community_FreeBoard";
+	}
 }
