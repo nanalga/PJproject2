3205e84d83afed940cb1f8eadd05f9084dfcd13e
Hesitater <feelksh0603@gmail.com>
2022-01-14 16:15:01 +0900

    2022-01-14 16:14
    2022-01-14 16:14

--- src/main/webapp/resources/js/address.js
+++ src/main/webapp/resources/js/address.js
@@ -1,14 +1,21 @@
 console.log("address.js");
 
+
+/*
 window.onload = function(){
     document.getElementById("addressInput").addEventListener("click", function(){ //주소입력칸을 클릭하면
         //카카오 지도 발생
         new daum.Postcode({
             oncomplete: function(data) { //선택시 입력값 세팅
                 document.getElementById("addressInput").value = data.address; // 주소 넣기
-             	document.querySelector("input[name=price]").focus();
+
+				document.querySelector("input[name=price]").focus();
 			//   document.querySelector("input[name=addressDetail]").focus(); //상세입력 포커싱
             }
+	
+
         }).open();
     });
-}
+
+
+}*/
